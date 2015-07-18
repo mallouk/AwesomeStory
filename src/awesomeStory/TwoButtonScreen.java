2a0c6a54c2cea5b3e2c1af8fed5e2d6237b6e340
Matthew Jallouk <jalloukmat@yahoo.com>
2015-07-17 20:05:38 -0400

    Added some code.

--- src/awesomeStory/TwoButtonScreen.java
+++ src/awesomeStory/TwoButtonScreen.java
@@ -1,5 +1,11 @@
 package awesomeStory;
 
-public class TwoButtonScreen {
+import javax.swing.JButton;
 
+public class TwoButtonScreen {
+	private JButton button1;
+	
+	public static void main(String[] args){
+		
+	}
 }
